e916e5c88e320e4edafa21e75737d4b31b85eec9
Timmerman <Z835133@umcn.nl>
2018-07-02 12:49:07 +0200

    alert

--- R/dateRange.R
+++ R/dateRange.R
@@ -9,11 +9,15 @@ renderDateRangeDim <- function(env,dim,input,output)  {
         dd <- env$dims[[dim]]
         
         s <- makeDateRangeSelection(env,dim,sel[1],sel[2])
-          
-        if(!identical(s,dd$selected)) {
-            dd$selected <- s
-            dd$selectSource <- 'dateRangeClick'
-            dd$reactive$selectChange <- dd$reactive$selectChange + 1
+        
+        if (is.null(s)) {
+            shinyjs::alert('Geen data!')
+        } else {
+            if(!identical(s,dd$selected)) {
+                dd$selected <- s
+                dd$selectSource <- 'dateRangeClick'
+                dd$reactive$selectChange <- dd$reactive$selectChange + 1
+            }
         }
         
     })
@@ -26,6 +30,9 @@ makeDateRangeSelection <- function(env,dim,from,to) {
     
     dt <- dd$data[['level1Label']]
     dt <- dt[dt >= from & dt <= to]
-    data.frame(level = 1, parent = root, label = dt, stringsAsFactors = FALSE)
+    if (length(dt) > 1)
+        data.frame(level = 1, parent = root, label = dt, stringsAsFactors = FALSE)
+    else 
+        NULL
 
 }
